262674f3813aeca77db59b4519c7314d0868c77c
h4-harshh <harshnew2204@gmail.com>
2024-09-15 19:49:05 +0530

    sidebar partially completed

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sidebar from './components/Sidebar/Sidebar.jsx'
+
+const App = () => {
+  return (
+    <>
+      <Sidebar/>
+    </>
+  )
+}
+
+export default App
